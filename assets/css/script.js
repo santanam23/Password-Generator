c5708c457ce39dd9d560e1a3937db847c8f4c736
santanam23 <mirmir11.ms@gmail.com>
2021-12-07 20:10:22 -0600

    created getelementById

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,4 +1,61 @@
-// Assignment code here
+//DOM elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+
+generateEl.addEventListener('click', () => {
+  const length = lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumbers = numbersEl.checked;
+  const hassymbol = SymbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower, 
+    hasUpper, 
+    hassymbol, 
+    hasNumbers, 
+    length
+    );
+});
+
+//Generate password function
+function generatePassword(lower,upper,numbers,symbol, length) {
+  //1. Init pw var
+  //2. Filter out unchecked types
+  //3. Loop Over length call generator function for each type 
+  // 4. Add final pw to the pw var and return
+
+}
+
+// Generator Functions Character chart http://www.net-comber.com/charset.html
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.ramdom() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.ramdom() * 26) + 97)
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.ramdom() * 10) + 48)
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}[]=/,.';
+  return symbols [Math.floor(Math.random() * symbols.length)];
 
 
 // Get references to the #generate element
@@ -12,6 +69,5 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
